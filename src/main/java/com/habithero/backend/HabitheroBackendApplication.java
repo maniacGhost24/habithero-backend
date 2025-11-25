ec5a5eeb26fbcac678ff177e08abb7dcbe1b125c
Rishabh Kaushik <sonusgnr34@gmail.com>
2025-11-25 22:18:06 +0530

    first commit

---
+++ src/main/java/com/habithero/backend/HabitheroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.habithero.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitheroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitheroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habithero/backend/HabitheroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habithero.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitheroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
